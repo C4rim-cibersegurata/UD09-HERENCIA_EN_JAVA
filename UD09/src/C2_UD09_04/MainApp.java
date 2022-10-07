319d50a9951832bbaa0e5cc4f1229a77bb6c7eef
Carl0sLafu <114726441+Carl0sLafu@users.noreply.github.com>
2022-10-07 12:36:58 +0200

    Update MainApp.java

--- UD09/src/C2_UD09_04/MainApp.java
+++ UD09/src/C2_UD09_04/MainApp.java
@@ -3,9 +3,10 @@ package C2_UD09_04;
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Raices solucion=new Raices (1,2,70);
 		solucion.calcular();
+		
 	}
 
 }
