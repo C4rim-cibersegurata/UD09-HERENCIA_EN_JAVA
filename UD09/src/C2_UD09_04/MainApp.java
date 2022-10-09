3cf013f618f0f7edc04ba40e01fb717fd9814aa5
Carl0sLafu <114726441+Carl0sLafu@users.noreply.github.com>
2022-10-09 20:53:30 +0200

    Update MainApp.java

--- UD09/src/C2_UD09_04/MainApp.java
+++ UD09/src/C2_UD09_04/MainApp.java
@@ -4,6 +4,9 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		
+		// Dos soluciones: 
+		// Una solución:
+		// Solucion irreal: 1, 3, 2
 		Raices solucion=new Raices (1,2,70);
 		solucion.calcular();
 		
