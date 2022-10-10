89bc0f8b8891e40cd20480afac9b81bac3fdc57e
C4rim-cibersegurata <114733605+C4rim-cibersegurata@users.noreply.github.com>
2022-10-10 08:35:40 +0200

    Create MainApp03.java

---
+++ UD09/src/C2_UD09_03/MainApp03.java
@@ -0,0 +1,14 @@
+package C2_UD09_04;
+
+public class MainApp03 {
+	public static void main(String [] args) {
+	Libro libro[]=new Libro[2];
+	libro[0]=new Libro("9785878468124","El nombre del viento","Patrick Rothfuss",873);
+	libro[1]=new Libro("9453615842547","Misery","Stephen King",400);
+	
+		for(int i=0;i<2;i++) {
+			System.out.println(libro[i].toString());
+		}
+	}
+}
+
