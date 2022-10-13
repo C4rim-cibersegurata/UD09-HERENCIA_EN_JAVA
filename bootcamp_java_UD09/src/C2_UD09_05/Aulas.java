70791847ca3d88d4bdecb228f0b22a948ade5e46
C4rim-cibersegurata <mendezluciano1999@gmail.com>
2022-10-13 05:27:50 +0200

    V1 ejercicio5. Falta arreglar un error y finalizar.

---
+++ bootcamp_java_UD09/src/C2_UD09_05/Aulas.java
@@ -0,0 +1,62 @@
+package C2_UD09_05;
+
+public class Aulas {
+	public int id;
+	public int MaxEstudiantes;
+	public String materia;
+
+	/**
+	 * Constructor 1 atributos
+	 */
+	public Aulas() {
+		this.id = 0;
+		this.MaxEstudiantes = 0;
+		this.materia = "";
+	}
+
+	/**
+	 * Constructor 2
+	 * 
+	 * @param id
+	 * @param MaxEstudiantes
+	 * @param materia
+	 */
+	public Aulas(int id, int MaxEstudiantes, String materia) {
+		this.id = id;
+		this.MaxEstudiantes = MaxEstudiantes;
+		this.materia = materia;
+
+	}
+
+	@Override
+	public String toString() {
+		return "Aulas [id=" + id + ", MaxEstudiantes=" + MaxEstudiantes + ", materia=" + materia + "]";
+	}
+
+	public int getIdAula() {// metodos get para devolver los valores de id, materia y MaxEstudiantes
+		return id;
+	}
+
+	public String getMateria() {
+		return materia;
+	}
+
+	public int getMaxEstudiantes() {
+		return MaxEstudiantes;
+	}
+	public boolean hayClase(Profesor profe, Estudiantes[] estudiantes) {
+		//if profe ha asistido, pasamos estudiantes  i con un for comprovamos por cada alumno si asistió
+		Profesor objProf = new Profesor();
+		boolean asistenciaHoy=true;
+		if (objProf.asistencia()) {
+			int alumnosPresentes = 0;
+			for(Estudiantes al : estudiantes) {
+				if(al.asistencia())
+					alumnosPresentes++;
+			}
+			int porcentajePresentes = alumnosPresentes*100/this.MaxEstudiantes;
+			if(porcentajePresentes>=50) {asistenciaHoy=true;}
+		} else {asistenciaHoy=true;}
+		return asistenciaHoy;
+		}
+}
---
+++ bootcamp_java_UD09/src/C2_UD09_05/Estudiantes.java
@@ -0,0 +1,39 @@
+package C2_UD09_05;
+
+import java.util.Random;
+
+public class Estudiantes extends ListaAsistencia {
+	public String nombre;
+    public int edad;
+    public String sexo;
+    public int nota;
+    /**
+     * Constructor 1 atributos
+     */
+    public Estudiantes() {
+        this.nombre = "";
+        this.edad = 0;
+        this.sexo = "";
+        this.nota = 0;
+    }
+    /**
+     * Constructor 2
+     * @param id 
+     * @param MaxEstudiantes
+     * @param materia
+     */
+    public Estudiantes(String nombre, int edad, String sexo, int nota) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.nota = nota;
+        
+    }
+    public int getNota() {
+		return nota;
+	}
+	public boolean asistencia() {//probabilidad del 50% de faltar a clase
+        Random rand = new Random(); 	
+        return rand.nextInt(100)>=50?true:false;
+	}
+}
